885d7503ec34505cbabd82718d6a666dca10b02c
priyanka shaw <priyankashaw680@gmail.com>
2024-03-21 23:26:43 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,44 @@
-import LanguageUi from "./assets/components/LanguageTranslatorPage.jsx/TranslatoeUi"
+import axios from 'axios'
 import './App.css'
-import { useEffect, useState } from "react"
-import axios from "axios"
+
 function App() {
- //creating states
- const [from , setFrom] = useState("");
- const [to , setTo] = useState("");
- const [fromInput , setFromInput] = useState("");
- const [toOutput , setToOutput] = useState("");
 
 
 
-
-
-//  const encodedParams = new URLSearchParams();
-//  encodedParams.set('source_language', 'en');
-//  encodedParams.set('target_language', 'id');
-//  encodedParams.set('text', 'What is your name?');
- 
-//  const options = {
-//    method: 'POST',
-//    url: 'https://text-translator2.p.rapidapi.com/translate',
-//    headers: {
-//      'content-type': 'application/x-www-form-urlencoded',
-//      'X-RapidAPI-Key': '420f80fab0mshe404d58ebeec317p173d3fjsndbaad82e0aa5',
-//      'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
-//    },
-//    data: encodedParams,
-//  };
- 
-//  try {
-//    const response =  axios.request(options);
-//    console.log(response.data);
-//  } catch (error) {
-//    console.error(error);
-//  }
-
-
- async function getTranslatedData() {
-  try {
-    // to convert from string to encoded format we use URLSearchParams
-   // const encodeData = new URLSearchParams();
-    // append the data with key and value
-    // encodeData.append("source_language", sourceLanguage);
-    // encodeData.append("target_language", targetLanguage);
-    // encodeData.append("text", data);
-
-    
-    const res = await axios.request(option);
-    //setResult(res.data.data.translatedText);
-    console.log(res)
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-
-const option = {
-  method: "post",
-  url: "https://text-translator2.p.rapidapi.com/translate",
-  headers: {
-    "content-type": "application/x-www-form-urlencoded",
-    "X-RapidAPI-Key":
-      "61fe42194dmsha3e696751cf48edp14633fjsn5dd03c4a1e74",
-    "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
-  },
-//  data: encodeData, // payload // request body
-};
-useEffect(()=>{
-    const data = axios.get(option).then((datarec)=>console.log(datarec))
-})
-
-
-  return (
+  return(
     <>
-      <LanguageUi/>
+        <div className=" container w-full h-screen bg-slate-200">
+           <div className="optionBoxContainer">
+                <div className="from">
+                   <div className="fromSelect">
+                        <select>
+                            <option>car</option>
+                            <option>auddi</option>
+                        </select>
+                   </div>
+                    <textarea rows="10" cols="60"></textarea>
+                </div>
+                <div className="to">
+                    <div className="ToSelect">
+                        <select>
+                            <option>car</option>
+                            <option>auddi</option>
+                        </select>
+                   </div>
+                    <textarea rows="10" cols="60"></textarea>
+                </div>
+                <div className="button">
+                <button>Translate</button>
+            </div>
+            </div>
+           <div className="button">
+                <button>Translate</button>
+            </div>
+
+        </div>
     </>
-  )
+)
+
 }
 
-export default App
+export default App;
--- src/assets/components/LanguageTranslatorPage.jsx/TranslatoeUi.jsx
+++ src/assets/components/LanguageTranslatorPage.jsx/TranslatoeUi.jsx
@@ -1,37 +1,5 @@
 
 function LanguageUi(){
-    return(
-        <>
-            <div className=" container w-full h-screen bg-slate-200">
-               <div className="optionBoxContainer">
-                    <div className="from">
-                       <div className="fromSelect">
-                            <select>
-                                <option>car</option>
-                                <option>auddi</option>
-                            </select>
-                       </div>
-                        <textarea rows="10" cols="60"></textarea>
-                    </div>
-                    <div className="to">
-                        <div className="ToSelect">
-                            <select>
-                                <option>car</option>
-                                <option>auddi</option>
-                            </select>
-                       </div>
-                        <textarea rows="10" cols="60"></textarea>
-                    </div>
-                    <div className="button">
-                    <button>Translate</button>
-                </div>
-                </div>
-               <div className="button">
-                    <button>Translate</button>
-                </div>
-
-            </div>
-        </>
-    )
+   
 }
 export default LanguageUi;
