2ac7e4312407b8d0b01231b811295086f2b5621f
priyanka shaw <priyankashaw680@gmail.com>
2024-03-22 00:46:19 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
 import axios from 'axios'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+// const [data , SetData] = useState("hello");
+// const [from , setFrom] = useState("en");
+// const [to , setTo]  = useState("hi");
+// const [input , setInput] = useState("");
+// const [output , setOutput] = useState(" ");
 
+const [data, setData] = useState("hello");
+  const [result, setResult] = useState("");
+  const [sourceLanguage, setSourceLanguage] = useState("en");
+  const [targetLanguage, setTargetLanguage] = useState("bn");
+//Api Calling 
+
+async function getTranslatedData(){
+
+  try{
+    const encodeData = new URLSearchParams();
+    encodeData.append("source_language" , sourceLanguage);
+    encodeData.append("target_language" , targetLanguage);
+    encodeData.append("text" , data);
+    // encodeData.append("source_language", sourceLanguage);
+    // encodeData.append("target_language", targetLanguage);
+    // encodeData.append("text", data);
+    const option = {
+      method : "post",
+      url: 'https://text-translator2.p.rapidapi.com/translate',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Key': '420f80fab0mshe404d58ebeec317p173d3fjsndbaad82e0aa5',
+        'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+      },
+      data: encodeData,
+    }
+    const response = await axios.request(option);
+    console.log(response)
+  }  
+  catch(error){
+    console.log(error)
+  }
+
+  }
 
 
   return(
@@ -28,12 +68,12 @@ function App() {
                     <textarea rows="10" cols="60"></textarea>
                 </div>
                 <div className="button">
-                <button>Translate</button>
+                <button onClick={()=>{
+                  getTranslatedData()
+                }}>Translate</button>
             </div>
             </div>
-           <div className="button">
-                <button>Translate</button>
-            </div>
+          
 
         </div>
     </>
@@ -42,3 +82,8 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
